869b5b7cf77ea8a80d4a8ef22087118c022e8e82
HSIEH, Li-Yi <lyshie@tn.edu.tw>
2017-08-01 23:17:48 +0800

    Add a script to purge

---
+++ purge_eic.cmd
@@ -0,0 +1,20 @@
+@echo 自動清除台南市公文系統
+@echo off
+
+REM 關閉通訊錄
+taskkill /im "Comp.exe" /f >nul 2>&1
+
+REM 完整移除公文系統
+wmic product where name="文書編輯-公文製作系統" call uninstall >nul 2>&1
+"%ProgramFiles(x86)%\公文管理系統：IE環境設定\uninstall.exe" /U:"%ProgramFiles(x86)%\公文管理系統：IE環境設定\Uninstall\uninstall.xml"
+"%ProgramFiles%\公文管理系統：IE環境設定\uninstall.exe" /U:"%ProgramFiles%\公文管理系統：IE環境設定\Uninstall\uninstall.xml"
+
+REM 刪除未自動移除的檔案
+del "%windir%\System32\eicdocn.dll"   >nul 2>&1
+del "%windir%\System32\eicsecure.dll" >nul 2>&1
+del "%windir%\System32\eicsign.dll"   >nul 2>&1
+del "%windir%\SysWOW64\eicdocn.dll"   >nul 2>&1
+del "%windir%\SysWOW64\eicsecure.dll" >nul 2>&1
+del "%windir%\SysWOW64\eicsign.dll"   >nul 2>&1
+
+rmdir /s /q "c:\eic"
