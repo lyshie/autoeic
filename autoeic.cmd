287a8d107350e4ce87488c4974fc28f28437b94b
HSIEH, Li-Yi <lyshie@tn.edu.tw>
2017-08-01 23:29:59 +0800

    Remove more files

--- autoeic.cmd
+++ autoeic.cmd
@@ -18,16 +18,18 @@ taskkill /im "Comp.exe" /f >nul 2>&1
 
 REM 完整移除公文系統
 wmic product where name="文書編輯-公文製作系統" call uninstall >nul 2>&1
-"%ProgramFiles(x86)%\公文管理系統：IE環境設定\uninstall.exe" /U:"%ProgramFiles(x86)%\公文管理系統：IE環境設定\Uninstall\uninstall.xml"
-"%ProgramFiles%\公文管理系統：IE環境設定\uninstall.exe" /U:"%ProgramFiles%\公文管理系統：IE環境設定\Uninstall\uninstall.xml"
+"%ProgramFiles(x86)%\公文管理系統：IE環境設定\uninstall.exe" /U:"%ProgramFiles(x86)%\公文管理系統：IE環境設定\Uninstall\uninstall.xml" >nul 2>&1
+"%ProgramFiles%\公文管理系統：IE環境設定\uninstall.exe" /U:"%ProgramFiles%\公文管理系統：IE環境設定\Uninstall\uninstall.xml" >nul 2>&1
 
 REM 刪除未自動移除的檔案
 del "%windir%\System32\eicdocn.dll"   >nul 2>&1
 del "%windir%\System32\eicsecure.dll" >nul 2>&1
 del "%windir%\System32\eicsign.dll"   >nul 2>&1
+del "%windir%\System32\eicpdf.dll"    >nul 2>&1
 del "%windir%\SysWOW64\eicdocn.dll"   >nul 2>&1
 del "%windir%\SysWOW64\eicsecure.dll" >nul 2>&1
 del "%windir%\SysWOW64\eicsign.dll"   >nul 2>&1
+del "%windir%\SysWOW64\eicpdf.dll"    >nul 2>&1
 
 REM 處理通訊錄異常問題
 ren "%adbook%\tncg"  "tncg_%datetime%"  >nul 2>&1
